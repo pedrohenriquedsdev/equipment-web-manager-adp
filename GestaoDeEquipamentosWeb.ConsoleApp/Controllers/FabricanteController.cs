047507aaf1ac21785a6a454b35e44bc728f97d72
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-13 17:01:32 -0300

    feat: implement Manufacturer listing

--- GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -18,32 +18,10 @@ public class FabricanteController : Controller
         repositorioFabricante = new RepositorioFabricanteEmArquivo(contexto);
     }
 
-    [HttpGet]
-
     public ActionResult Listar()
     {
         List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
 
         return View(fabricantes);
     }
-
-    [HttpGet]
-
-    public ActionResult Cadastrar()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public ActionResult Cadastrar(string nome, string email, string telefone)
-    {
-        Fabricante novoFabricante = new Fabricante(nome, email, telefone);
-
-        repositorioFabricante.Cadastrar(novoFabricante);
-
-        string listarStr = nameof(Listar);
-
-        return RedirectToAction("Listar");
-    }
 }
-
