6c6f39ceed29b3560405b9d740fa82d10bcf98b9
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-19 11:04:44 -0300

    chore: reset v0 to initial state for code review and re-learning

--- GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//ASP.NET Core -> Entra para evitar detalhes de baixo nível extremo (web app)
+//Vai atuar como nossa TelasBase de antes
+
+//Builder de um web server
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); // -> // tipagem de app web (como iremos organizar as apresentações) Models, Controllers & Views
+
+//Criação da instância do web server
+WebApplication app = builder.Build();
+
+//Middlewares - Funções que executam em cada chamada que o nosso server vai receber
+app.UseRouting(); // considera a chamada do server e + as rotas dele
+app.MapDefaultControllerRoute(); //rotas específicas
+
+// Iniciar o loop da app 
+app.Run();
+
