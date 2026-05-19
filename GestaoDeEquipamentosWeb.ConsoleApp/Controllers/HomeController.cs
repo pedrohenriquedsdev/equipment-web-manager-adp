df33171ffabcd74af8917b50cfc2eaef53ff6017
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-19 11:49:34 -0300

    feat: add HomeController and Index view with basic structure

---
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentosWeb.ConsoleApp.Controllers
+
+//MVC - Model, View & Controller
+
+
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() //isso é uma ação chamada index (a página inicial de um controller)
+        {
+            return View();
+        }
+    }
+}
