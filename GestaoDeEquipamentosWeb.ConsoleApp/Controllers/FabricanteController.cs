5d778367b82553bf02e30db2ede61539bce3ea7e
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-19 12:28:59 -0300

    feat: add manufacturer registration

--- GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -3,25 +3,42 @@ using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado.Arquivos;
 using GestaoDeEquipamentosWeb.ConsoleApp.ModuloFabricante;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GestaoDeEquipamentosWeb.ConsoleApp.Controllers
+namespace GestaoDeEquipamentosWeb.ConsoleApp.Controllers;
+
+public class FabricanteController : Controller
 {
-    public class FabricanteController : Controller
+    private readonly IRepositorio<Fabricante> repositorioFabricante;
+
+    public FabricanteController()
     {
-        private readonly IRepositorio<Fabricante> repositorioFabricante;
+        ContextoJson contexto = new ContextoJson();
+        contexto.Carregar();
 
-        public FabricanteController()
-        {
-            ContextoJson contexto = new ContextoJson();
-            contexto.Carregar();
+        repositorioFabricante = new RepositorioFabricanteEmArquivo(contexto);
+    }
 
-            repositorioFabricante = new RepositorioFabricanteEmArquivo(contexto);
-        }
+    [HttpGet]
+    public IActionResult Listar()
+    {
+        List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
 
-        public IActionResult Listar()
-        {
-            List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
+        return View(fabricantes);
+    }
 
-            return View(fabricantes);
-        }
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(string nome, string email, string telefone)
+    {
+        Fabricante novoFabricante = new Fabricante(nome, email, telefone);
+
+        repositorioFabricante.Cadastrar(novoFabricante);
+
+        return RedirectToAction(nameof(Listar));
     }
 }
+
