d74aaea6aa3dd11ae7ce337692c4ef03ed45ad84
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-19 12:18:26 -0300

    feat: add manufacturer listing

---
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -0,0 +1,27 @@
+﻿using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado.Arquivos;
+using GestaoDeEquipamentosWeb.ConsoleApp.ModuloFabricante;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentosWeb.ConsoleApp.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        private readonly IRepositorio<Fabricante> repositorioFabricante;
+
+        public FabricanteController()
+        {
+            ContextoJson contexto = new ContextoJson();
+            contexto.Carregar();
+
+            repositorioFabricante = new RepositorioFabricanteEmArquivo(contexto);
+        }
+
+        public IActionResult Listar()
+        {
+            List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
+
+            return View(fabricantes);
+        }
+    }
+}
